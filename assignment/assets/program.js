b2914b10d4c4c70463b599e24cabb9662d789760
sigma <sigma0913@outlook.jp>
2026-05-27 16:52:39 +0900

    append before and css

--- assignment/assets/program.js
+++ assignment/assets/program.js
@@ -1,5 +1,37 @@
 fetch("https://script.google.com/macros/s/AKfycbzFaGMSeXie9yUBJlxTHmE2GWvh9fJRcBNMgtlH595IhLYtybr2mrCIB3LP3txz5Us7lA/exec")
   .then(r => r.json())
   .then(data => {
-    console.log(data);
+    for (let i = 0; i < data.length; i++) {
+
+      let date = data[i][0].substr(0, 8);
+      
+      const yesterday = convertdate(new Date(new Date().setDate(new Date().getDate() - 1)));
+      const today = convertdate(new Date());
+      const tomorrow = convertdate(new Date(new Date().setDate(new Date().getDate() + 1)));
+
+      let formatdate = date == yesterday ? "昨日" : date == today ? "今日" : date == tomorrow ? "明日" : data[i][0].substr(0, 4) + "年" + data[i][0].substr(4, 2) + "月" + data[i][0].substr(6, 2) + "日";
+
+      if (data[i][1] !== "") {
+        if (data[i][0] <= toString(convertdate(new Date()) + converttime(new Date()))){
+          document.getElementById("assignment-table-before").innerHTML += `<tr>
+            <td>${data[i][1]}</td>
+            <td>${data[i][2]}</td>
+            <td>${formatdate}</td>
+          </tr>`;
+        } else {
+          document.getElementById("assignment-table-after").innerHTML += `<tr>
+            <td>${data[i][1]}</td>
+            <td>${data[i][2]}</td>
+            <td>${formatdate}</td>
+          </tr>`;
+        }
+      }
+    }
   });
+
+function convertdate(date) {
+  return date.getFullYear() + (date.getMonth() + 1 < 10 ? "0" + (date.getMonth() + 1) : date.getMonth() + 1) + (date.getDate() < 10 ? "0" + date.getDate() : date.getDate());
+}
+function converttime(date) {
+  return date.getHours() < 10 ? "0" + date.getHours() : date.getHours() + (date.getMinutes() < 10 ? "0" + date.getMinutes() : date.getMinutes());
+}
