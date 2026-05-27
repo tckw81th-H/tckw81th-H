5c2c516dfa52b3223539c4b6059c62c5ab94cdf2
sigma <sigma0913@outlook.jp>
2026-05-28 06:15:38 +0900

    changed

--- assignment/assets/program.js
+++ assignment/assets/program.js
@@ -9,20 +9,21 @@ fetch("https://script.google.com/macros/s/AKfycbzFaGMSeXie9yUBJlxTHmE2GWvh9fJRcB
       const today = convertdate(new Date());
       const tomorrow = convertdate(new Date(new Date().setDate(new Date().getDate() + 1)));
 
-      let formatdate = date == yesterday ? "昨日" : date == today ? "今日" : date == tomorrow ? "明日" : data[i][0].substr(0, 4) + "年" + data[i][0].substr(4, 2) + "月" + data[i][0].substr(6, 2) + "日";
+      let formatdate = date == yesterday ? "昨日" : date == today ? "今日" : date == tomorrow ? "明日" : data[i][0].substr(4, 2) + "/" + data[i][0].substr(6, 2);
 
       if (data[i][1] !== "") {
-        if (data[i][0] <= toString(convertdate(new Date()) + converttime(new Date()))){
+        console.log(data[i][0].substr(0, 8) + data[i][0].substr(9, 4), Number(convertdate(new Date()) + converttime(new Date())));
+        if (Number(data[i][0].substr(0, 8) + data[i][0].substr(9, 4)) <= Number(convertdate(new Date()) + converttime(new Date()))){
           document.getElementById("assignment-table-before").innerHTML += `<tr>
-            <td>${data[i][1]}</td>
-            <td>${data[i][2]}</td>
-            <td>${formatdate}</td>
+            <td class="name">${data[i][1]}</td>
+            <td class="subject">${data[i][2]}</td>
+            <td class="due-date">${formatdate}</td>
           </tr>`;
         } else {
           document.getElementById("assignment-table-after").innerHTML += `<tr>
-            <td>${data[i][1]}</td>
-            <td>${data[i][2]}</td>
-            <td>${formatdate}</td>
+            <td class="name">${data[i][1]}</td>
+            <td class="subject">${data[i][2]}</td>
+            <td class="due-date">${formatdate}</td>
           </tr>`;
         }
       }
@@ -30,8 +31,8 @@ fetch("https://script.google.com/macros/s/AKfycbzFaGMSeXie9yUBJlxTHmE2GWvh9fJRcB
   });
 
 function convertdate(date) {
-  return date.getFullYear() + (date.getMonth() + 1 < 10 ? "0" + (date.getMonth() + 1) : date.getMonth() + 1) + (date.getDate() < 10 ? "0" + date.getDate() : date.getDate());
+  return date.getFullYear() + String(date.getMonth() + 1).padStart(2, "0") + String(date.getDate()).padStart(2, "0");
 }
 function converttime(date) {
-  return date.getHours() < 10 ? "0" + date.getHours() : date.getHours() + (date.getMinutes() < 10 ? "0" + date.getMinutes() : date.getMinutes());
+  return String(date.getHours()).padStart(2, "0") + String(date.getMinutes()).padStart(2, "0");
 }
